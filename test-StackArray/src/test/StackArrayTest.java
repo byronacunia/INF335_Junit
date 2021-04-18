91ee98f61b28a91bee305892867583e43e83188e
Byron Alejandro Acuña Acurio <byronacunia@gmail.com>
2021-04-18 20:58:41 -0300

    Teste de tamanho inicial da pilha

--- test-StackArray/src/test/StackArrayTest.java
+++ test-StackArray/src/test/StackArrayTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,9 +10,34 @@ import algorithms.datastructures.StackArray;
 public class StackArrayTest {
 	
 	@Test
+	public void testInitialSize() {
+		StackArray myStack = new StackArray();
+		int tamanho = myStack.size();
+		boolean valid = (tamanho == 0);
+		assertTrue(valid); 		
+	}
+	
+	@Test
 	public void testValidisEmpty () {
-		StackArray pman = new StackArray();
-		boolean valid = pman.isEmpty();
+		StackArray myStack = new StackArray();
+		boolean valid = myStack.isEmpty();
+		assertTrue(valid); 		
+	}
+	
+	@Test
+	public void testIsNoEmpty () {
+		StackArray myStack = new StackArray();
+		myStack.push(5);
+		boolean valid = myStack.isEmpty();
+		assertFalse(valid); 		
+	}
+	
+	@Test
+	public void testPopPush () {
+		StackArray myStack = new StackArray();
+		myStack.push(5);
+		int dado = myStack.pop();
+		boolean valid = (dado == 5);
 		assertTrue(valid); 		
 	}
 }
